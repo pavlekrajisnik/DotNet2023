e1e55600fbbb05bb274d7516cbc73274923fb282
Pavle <pavlekrajisnik001@gmail.com>
2023-05-25 10:54:20 +0200

    Metoda AddPjesma

--- Services/PjesmaService.cs
+++ Services/PjesmaService.cs
@@ -86,5 +86,15 @@ namespace VirtualDj.Services
             Service_Response.Data = await _context.Pjesme.Select(c => _mapper.Map<GetPjesmaDto>(c)).ToListAsync();
             return Service_Response;
         }
+
+        public async Task<ServiceResponse<List<GetPjesmaDto>>> AddPjesma(AddPjesmaDto newpjesma)
+        {
+            var Service_Response = new ServiceResponse<List<GetPjesmaDto>>();
+            var pjesma = _mapper.Map<Pjesma>(newpjesma);
+             _context.Pjesme.Add(pjesma);
+            await _context.SaveChangesAsync();
+            Service_Response.Data = await _context.Pjesme.Select(c => _mapper.Map<GetPjesmaDto>(c)).ToListAsync();
+            return Service_Response;
+        }
     }
 }
